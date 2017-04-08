bc84fc68c766a78090bb086c000951843b741aa2
Radoslaw Stolarski <stolarz92@gmail.com>
2017-04-08 21:56:28 +0200

    Update controllers + tests + controllers concerns

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
@@ -75,5 +77,7 @@ RSpec.configure do |config|
       example.run
     end
   end
+
+  config.include RequestSpecHelper, type: :request
 end
 
