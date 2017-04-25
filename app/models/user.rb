d571d19957a292d88ff346982a37c113a24e1a9f
Radoslaw Stolarski <stolarz92@gmail.com>
2017-04-25 02:34:32 +0200

    Improvements

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   has_many :todos, foreign_key: :created_by
   # Validations
   validates_presence_of :name, :email, :password_digest
+  validates_uniqueness_of :name
 end
