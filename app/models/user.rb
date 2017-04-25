3e70eb0450d060dc686b4c510cf30dd74a64ea68
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2017-04-25 11:02:02 +0200

    Add uniqueness valdiation for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   has_many :todos, foreign_key: :created_by
   # Validations
   validates_presence_of :name, :email, :password_digest
-  validates_uniqueness_of :name
+  validates_uniqueness_of :email
 end
