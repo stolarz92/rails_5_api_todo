027e759b39b35783235d9bbfca88287edb111e78
stolarz <stolarz92@gmail.com>
2017-09-09 23:54:30 +0200

    Add annotate gem + add annotation

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: todos
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  description :text(65535)
+#  created_by  :string(255)
+#  done        :boolean          default(FALSE)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Todo < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :created_by
